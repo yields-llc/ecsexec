94a123ad6f18889e5f1a76b98b648d29f1575a2f
imunew <imunew@gmail.com>
2023-09-18 00:31:38 +0900

    the command is not terminated with an error if it is canceled with ctrl+c

--- internal/tasks/execute_command.go
+++ internal/tasks/execute_command.go
@@ -2,9 +2,11 @@ package tasks
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
 )
 
 type (
@@ -16,6 +18,9 @@ type (
 
 // Run is a method to execute this task.
 func (s *ExecuteCommand) Run(ctx context.Context) TaskResult {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
 	ctxValue := GetContextValue(ctx)
 	command := fmt.Sprintf(
 		`$(which aws) --profile %s ecs execute-command --cluster %s --task %s --container %s --interactive --command "%s"`,
@@ -25,15 +30,20 @@ func (s *ExecuteCommand) Run(ctx context.Context) TaskResult {
 		ctxValue.Container,
 		ctxValue.Command,
 	)
-	cmd := exec.Command(
-		"bash", "-c", command,
+	cmd := exec.CommandContext(
+		ctx, "bash", "-c", command,
 	)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Cancel = func() error {
+		return cmd.Process.Signal(os.Interrupt)
+	}
 	err := cmd.Run()
-	if err != nil {
+	if errors.Is(err, context.Canceled) {
+		return TaskResult{}
+	} else if err != nil {
 		return TaskResult{Error: err}
 	}
 
